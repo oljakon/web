a51fbcc46afcce0aff9bcdb1d64bf89c76552046
oljakon <41132114+oljakon@users.noreply.github.com>
2020-12-06 23:58:06 +0300

    update

--- web_lab1/catalog/admin.py
+++ web_lab1/catalog/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import City, Industry, Company, JobVacancy, Application, Resume
+from .models import City, Industry, Company, JobVacancy, Application
 
 admin.site.register(City)
 admin.site.register(Industry)
 admin.site.register(Company)
 admin.site.register(JobVacancy)
 admin.site.register(Application)
-admin.site.register(Resume)
